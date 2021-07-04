43746bb0a02b33c8dc4dda15121b0db9a5b74c31
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-04 20:54:12 +0300

    add tools

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import '../styles/canvas.scss'
+import {observer} from 'mobx-react-lite'
+import canvasState from '../store/canvasState'
+import toolState from '../store/toolState'
+import Brush from '../tools/Brush'
 
 function Canvas() {
+    const canvasRef = useRef()
+
+    useEffect(()=> {
+        canvasState.setCanvas(canvasRef.current)
+        toolState.setTool(new Brush(canvasRef.current))
+    }, [])
+
     return (
-        <div  className="canvas">
-            <canvas width={600} height={400}></canvas>
+        <div className="canvas">
+            <canvas width={600} height={400} ref={canvasRef}></canvas>
         </div>
     )
 }
 
-export default Canvas
+export default observer(Canvas)
--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import canvasState from '../store/canvasState'
+import toolState from '../store/toolState'
 import '../styles/toolbar.scss'
+import Brush from '../tools/Brush'
+import Rect from '../tools/Rect'
+import Circle from '../tools/Circle'
+import Eraser from '../tools/Eraser'
+import Line from '../tools/Line'
 
 function Toolbar() {
     return (
         <div className="toolbar">
-                <button className="toolbar__btn brush"></button>
-                <button className="toolbar__btn rect"></button>
-                <button className="toolbar__btn circle"></button>
-                <button className="toolbar__btn eraser"></button>
-                <button className="toolbar__btn line"></button>
-                <input type="color" style={{marginLeft: "10px"}}/>
-                <button className="toolbar__btn undo"></button>
-                <button className="toolbar__btn redo"></button>
-                <button className="toolbar__btn save"></button>
+            <button className="toolbar__btn brush" onClick={() => {toolState.setTool(new Brush(canvasState.canvas))}}/>
+            <button className="toolbar__btn rect" onClick={() => {toolState.setTool(new Rect(canvasState.canvas))}}/>
+            <button className="toolbar__btn circle" onClick={() => {toolState.setTool(new Circle(canvasState.canvas))}}/>
+            <button className="toolbar__btn eraser" onClick={() => {toolState.setTool(new Eraser(canvasState.canvas))}}/>
+            <button className="toolbar__btn line" onClick={() => {toolState.setTool(new Line(canvasState.canvas))}}/>
+            <input type="color" style={{marginLeft: "10px"}}/>
+            <button className="toolbar__btn undo"/>
+            <button className="toolbar__btn redo"/>
+            <button className="toolbar__btn save"/>
         </div>
     )
 }
