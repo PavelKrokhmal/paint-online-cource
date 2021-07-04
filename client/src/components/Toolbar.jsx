2df48a10dcb67f8d12767beb948adf2cae48c335
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-04 18:28:43 +0300

    toolbar buttons

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -4,9 +4,15 @@ import '../styles/toolbar.scss'
 function Toolbar() {
     return (
         <div className="toolbar">
-            <div>
-                Toolbar
-            </div>
+                <button className="toolbar__btn brush"></button>
+                <button className="toolbar__btn rect"></button>
+                <button className="toolbar__btn circle"></button>
+                <button className="toolbar__btn eraser"></button>
+                <button className="toolbar__btn line"></button>
+                <input type="color" style={{marginLeft: "10px"}}/>
+                <button className="toolbar__btn undo"></button>
+                <button className="toolbar__btn redo"></button>
+                <button className="toolbar__btn save"></button>
         </div>
     )
 }
